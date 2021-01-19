629447851bb1cbb02e0cc5af2d1abe66ab8da980
Dmitriy Satymov <oksdim@mail.ru>
2021-01-19 21:50:19 +0300

    error=216

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -3,7 +3,12 @@ package day0;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("It doesn't  work with win 32!");
-        
-    }
+        System.out.println("It doesn't work with win 32!");
+            }
 }
+
+  /*  Cannot run program "C:\Users\Диана\.jdks\openjdk-15.0.1\bin\java.exe" (in directory "C:\Users\Диана\AppData\Local\JetBrains\IdeaIC2020.3\compile-server"):
+        CreateProcess error=216, Версия "%1" не совместима с версией Windows, работающей на этом компьютере.
+        Проверьте сведения о системе, чтобы узнать, какая версия программы x86 (32-разрядная) или x64 (64-разрядная) вам нужна,
+        и получите ее у поставщика программного обеспечения
+*/
