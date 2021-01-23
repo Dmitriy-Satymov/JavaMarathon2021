ce18707d930d267d9c47e1a20232bf80016120cd
Dmitriy Satymov <oksdim@mail.ru>
2021-01-23 22:24:00 +0300

    Add Day3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
+import java.util.Scanner;
 
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main (String args[]){
+        Scanner s=new Scanner(System.in);
 
+
+        for (;;) {
+            double a = s.nextDouble();
+            double b = s.nextDouble();
+            if (b==0) break;
+
+            System.out.println(a/b);
+
+
+        }
     }
 }
+
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
+import java.util.Scanner;
 
-public class Task3 {
-    public static void main(String[] args) {
+public class day3_task3 {
+    public static void main (String args[]){
+
+        Scanner s=new Scanner(System.in);
+
+        for (int i = 1; i < 6; i++) {
+
+            double a=s.nextInt();
+            double b=s.nextDouble();
+
+            if (b==0) {System.out.println("Деление на 0");continue;}
+            System.out.println(i+"-"+a/b);
+
+
+        }
 
     }
 }
